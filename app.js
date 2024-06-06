b6f379754e67ae7bacd6533b34c991af00737e20
zuzingcebo <143517969+zuzingcebo@users.noreply.github.com>
2024-06-06 14:19:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ hamburger.addEventListener("click", () => {
 document.addEventListener("scroll", () => {
   var scroll_position = window.scrollY;
   if (scroll_position > 250) {
-    header.style.backgroundColor = "#29323c";
+    header.style.backgroundColor = "#000000";
   } else {
-    header.style.backgroundColor = "transparent";
+    header.style.backgroundColor = "#00000000";
   }
 });
 
